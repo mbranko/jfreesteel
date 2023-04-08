a7e1908e3c1dfa778964b8e4f66e319b32e63eb3
mbranko <mbranko@uns.ac.rs>
2023-04-08 12:41:04 +0200

    Bug fixes for card 3

--- jfreesteel/src/main/java/net/devbase/jfreesteel/EidCardSmartCafe.java
+++ jfreesteel/src/main/java/net/devbase/jfreesteel/EidCardSmartCafe.java
@@ -1,7 +1,12 @@
 package net.devbase.jfreesteel;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
 import javax.smartcardio.Card;
 import javax.smartcardio.CardException;
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
 
 /**
  * Smart card wrapper for the third generation (2021-) Serbian eID card.
@@ -20,7 +25,7 @@ import javax.smartcardio.CardException;
  * @author Goran Rakic (grakic@devbase.net)
  */
 @SuppressWarnings("restriction") // Various javax.smartcardio.*
-public class EidCardSmartCafe extends EidCardGemalto {
+public class EidCardSmartCafe extends EidCard {
 
     /** The list of known card ATRs, used to identify this smartcard. */
     public static final byte[] CARD_ATR = {
@@ -29,7 +34,57 @@ public class EidCardSmartCafe extends EidCardGemalto {
         (byte) 0x47, (byte) 0x43, (byte) 0x4E, (byte) 0x33, (byte) 0x5E
     };
 
+    /** Factory "selection" method */
+    protected static boolean isKnownAtr(byte[] atrBytes) {
+        return Arrays.equals(atrBytes, CARD_ATR);
+    }
+
+    static final byte[] LICNA_KARTA_AID = {
+        (byte) 0xF3, (byte) 0x81, (byte) 0x00, (byte) 0x00, (byte) 0x02, (byte) 0x53, (byte) 0x45, 
+        (byte) 0x52, (byte) 0x49, (byte) 0x44, (byte) 0x01
+    };
+
     protected EidCardSmartCafe(Card card) throws CardException {
         super(card);
+
+        // Select aid
+        ResponseAPDU response = channel.transmit(
+                new CommandAPDU(0x00, 0xA4, 0x04, 0x00, LICNA_KARTA_AID));
+        if (response.getSW() != 0x9000) {
+            throw new CardException(
+                    String.format("Select AID failed: status=%s", 
+                            Utils.int2HexString(response.getSW())));
+        }        
     }
+
+    protected byte[] readElementaryFile(final byte[] name, boolean strip_tag) throws CardException {
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        byte[] fileinfo = selectFile(name, 4);
+
+        // length hint from file info
+        int length = ((0xFF&fileinfo[2])<<8) + (0xFF&fileinfo[3]);
+        int offset = 0;
+        boolean known_real_length = false;
+
+        while (length > 0) {
+            byte[] data = readBinary(offset, length);
+
+            if (!known_real_length) {
+                // get length from outher tag, skip first 4 bytes (outher tag + length)
+                // if strip_tag is true, skip 4 more bytes (inner tag + length) and return content 
+                length = ((0xFF&data[3])<<8) + (0xFF&data[2]);
+                int skip = strip_tag ? 8 : 4;
+                out.write(data, skip, data.length-skip);
+                known_real_length = true;
+            }
+            else out.write(data, 0, data.length);
+
+            offset += data.length;
+            length -= data.length;
+        }
+
+        return out.toByteArray();
+    }   
 }
